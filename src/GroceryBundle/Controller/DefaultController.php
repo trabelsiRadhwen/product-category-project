19e4f9313e1f7d1cbc301711a7d3bf3f13f148d2
trabelsi radhwen <trabelsi.radhouen@gmail.com>
2018-10-23 11:18:17 +0100

    first commit

---
+++ src/GroceryBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GroceryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('GroceryBundle:Default:index.html.twig');
+    }
+}
---
+++ src/GroceryBundle/GroceryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GroceryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GroceryBundle extends Bundle
+{
+}
