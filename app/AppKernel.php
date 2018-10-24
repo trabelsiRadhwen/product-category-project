bbe473a0b42e30f8a8b5a3660ed3878970bd33dd
trabelsi radhwen <trabelsi.radhouen@gmail.com>
2018-10-24 11:54:55 +0100

    added bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,10 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new GroceryBundle\GroceryBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new ApiBundle\ApiBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
